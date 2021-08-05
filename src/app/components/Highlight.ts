0caba1c7a81e0e50f535e73a1af91a3b9cd37910
José Ignacio Cruz Moreira <joselove2code@gmail.com>
2021-08-05 18:48:43 -0400

    Add prop to display as block.

--- src/app/components/Highlight.ts
+++ src/app/components/Highlight.ts
@@ -2,11 +2,13 @@ import styled from "styled-components";
 
 type Props = {
   bold?: boolean;
+  block?: boolean;
 };
 
 export const Hightlight = styled.span<Props>`
   color: var(--highlightColor);
   font-weight: ${({ bold }) => bold ? 'bold' : 'normal' };
+  display: ${({ block }) => block ? 'block' : 'inline' };
 
   ::selection {
     color: var(--backgroundColor);
